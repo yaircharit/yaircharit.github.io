bbd1334a2b13ef13762096858c6e7fcd6e341fec
Yair Charit <yaircharit@gmail.com>
2020-11-28 23:46:15 +0200

    Modern Design Update:
    [FEAT]	set a rough template for Landing page
    [FIX] 	social icons animation changes vertical position
    [FEAT]	added resume as pdf (navbar)
    
    Signed-off-by: Yair Charit <yaircharit@gmail.com>

--- YCtemplate.js
+++ YCtemplate.js
@@ -1,7 +1,7 @@
 //TODO: set styles using CSSClasses
 const socialbar =   '<table>'+
-                        '<tr>'+
-                            '<td><a href="https://LinkedIn/in/yair-charit"><img id="linkedinIcon" src="Art/icons/linkedin.png" class="thumbnail img-rounded social-icon"></a></td>'+
+                        '<tr style="height:70px">'+
+                            '<td><a href="https://LinkedIn.com/in/yair-charit"><img id="linkedinIcon" src="Art/icons/linkedin.png" class="thumbnail img-rounded social-icon"></a></td>'+
                             '<td><a href="mailto:yaircharit@gmail.com"><img id="emailIcon" src="Art/icons/email.png"class="thumbnail img-rounded social-icon"></a></td>'+
                             '<td><a href="https://github.com/yaicharit"><img id="githubIcon" src="Art/icons/github.png"class="thumbnail img-rounded social-icon"></a></td>'+
                             '<td><a href="tel:+972-54-440-8838"><img id="phoneIcon" src="Art/icons/phone.png" class="thumbnail img-rounded social-icon"></a></td>'+
@@ -11,26 +11,27 @@ const socialbar =   '<table>'+
 
 const navbar = ''+
 '<div id="navbar" class="navbar navbar-fixed-top" style="background-color: transparent;height: 62px; position:fixed;">'+
-    '<div id="navbarBackground" class="colors" style="width:100%; height:62px; position:fixed; opacity:0; z-index:-99;"> </div>'+
-    '<div class="container"  style="margin-top:5px; width: 100%">'+
+    '<div id="navbarBackground" class="colors" style="width:100%; height:65px; position:fixed; opacity:0; z-index:-99;"> </div>'+
+    '<div class="container"  style=" width: 100%">'+
         '<div id="navSocial" style="opacity: 0; margin-right:0; float:right">'+
             socialbar+
         '</div>'+
         '<div style="margin-left: 0">'+
             '<div class="navbar-header" >'+
-                '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">'+
-                '<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>'+
-                '<a class="img-thumbnail img-circle" href="index.html"><img src="Art/YCLogo/YC_Logo_DarkRed2.png" alt="YCLogo" height="35"/></a>'+
+                // '<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>+
+                '<button type="button" class="navbar-toggle navbar-nav" data-toggle="collapse" data-target=".navbar-collapse" style="float: left; margin-top:0; display: block;">'+
+                '<a class="img-thumbnail img-circle" style="background-color:transparent; "><img src="Art/YCLogo/YC_Logo_DarkRed2.png" alt="YCLogo" height="35px"/></a></button>'+
             '</div>'+
             '<div class="collapse navbar-collapse" id="myNavbar">'+
-                '<ul class="nav navbar-nav">'+
-                    '<li><a href="index.html" style="color:white; background-color: transparent;">About</a></li>'+
-                    '<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="projects.html" style="color:white; background-color: transparent;">Personal Projects<span class="caret"></span></a>'+
+                '<ul id="toggle-navbar" class="nav navbar-nav ">'+
+                    '<li><a href="index.html" style="color:white; background-color:transparent;">About</a></li>'+
+                    '<li class="dropdown "><a class="dropdown-toggle" data-toggle="dropdown" href="projects.html" style="color:white; background-color: transparent;">Projects<span class="caret"></span></a>'+
                         '<ul class="dropdown-menu">'+
                             '<li><a href="rps.html">RPS CTF</a></li>'+
                             // '<li><a href="usefull.html">usefull files</a></li>'+
                         '</ul>'+
                     '</li>'+
+                    '<li><a href="YC_CV2020.pdf" target="parent" style="color:white; background-color:transparent;">Resume</a></li>'+
                     // '<li><a href="workRefs.html">Work Projects</a></li>'+
                 '</ul>'+
             '</div>'+
