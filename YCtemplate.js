f87c033136766ef486ad5bc82a6987544cf8db42
Yair Charit <yaircharit@gmail.com>
2020-11-28 20:03:05 +0200

    Added gradient animation to firstDiv and navbar

--- YCtemplate.js
+++ YCtemplate.js
@@ -1,36 +1,40 @@
-const socialbar =   '<table style="border-collapse: separate; border-spacing: 20px;">'+
+//TODO: set styles using CSSClasses
+const socialbar =   '<table>'+
                         '<tr>'+
-                            '<td><a href="https://LinkedIn/in/yair-charit"><img src="Art/icons/linkedin.png" class="thumbnail img-rounded" style="height: 35px; border-radius: 25px; border: 1px solid black;"></a></td>'+
-                            '<td><a href="mailto:yaircharit@gmail.com"><img src="Art/icons/email.png"class="thumbnail img-rounded"style="height: 35px; border-radius: 25px; border: 1px solid black;" /></a></td>'+
-                            '<td><a href="https://github.com/yaicharit"><img src="Art/icons/github.png"class="thumbnail img-rounded"style="height: 35px; border-radius: 25px; border: 1px solid black;" /></a></td>'+
-                            '<td><a href="tel:+972-54-440-8838"><img src="Art/icons/phone.png" class="thumbnail img-rounded" style="height: 35px; border-radius: 25px; border: 1px solid black;" /></a></td>'+
+                            '<td><a href="https://LinkedIn/in/yair-charit"><img id="linkedinIcon" src="Art/icons/linkedin.png" class="thumbnail img-rounded social-icon"></a></td>'+
+                            '<td><a href="mailto:yaircharit@gmail.com"><img id="emailIcon" src="Art/icons/email.png"class="thumbnail img-rounded social-icon"></a></td>'+
+                            '<td><a href="https://github.com/yaicharit"><img id="githubIcon" src="Art/icons/github.png"class="thumbnail img-rounded social-icon"></a></td>'+
+                            '<td><a href="tel:+972-54-440-8838"><img id="phoneIcon" src="Art/icons/phone.png" class="thumbnail img-rounded social-icon"></a></td>'+
+                            '<td><a href="https://api.whatsapp.com/send?phone=972544408838"><img id="whatsappIcon" src="Art/icons/whatsapp.png" class="thumbnail img-rounded social-icon"></a></td>'+
                         '</tr>'+
                     '</table>'
 
 const navbar = ''+
-'<div class="navbar navbar-fixed-top" style="background-color: white; color: black;height: 62px">'+
-    '<div class="container">'+
-        '<div id="navSocial" style="float:right; opacity: 0">'+
+'<div id="navbar" class="navbar navbar-fixed-top" style="background-color: transparent;height: 62px; position:fixed;">'+
+    '<div id="navbarBackground" class="colors" style="width:100%; height:62px; position:fixed; opacity:0; z-index:-99;"> </div>'+
+    '<div class="container"  style="margin:5px;">'+
+        '<div id="navSocial" style="float:right; opacity: 0; ">'+
             socialbar+
         '</div>'+
-        '<div class="navbar-header">'+
-            '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">'+
-            '<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>'+
-            '<a class="img-thumbnail img-circle" href="index.html"><img src="Art/YCLogo/YC_Logo_DarkRed2.png" alt="YCLogo" height="35"/></a>'+
+        '<div >'+
+            '<div class="navbar-header" >'+
+                '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">'+
+                '<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>'+
+                '<a class="img-thumbnail img-circle" href="index.html"><img src="Art/YCLogo/YC_Logo_DarkRed2.png" alt="YCLogo" height="35"/></a>'+
+            '</div>'+
+            '<div class="collapse navbar-collapse" id="myNavbar">'+
+                '<ul class="nav navbar-nav">'+
+                    '<li><a href="index.html" style="color:white; background-color: transparent;">About</a></li>'+
+                    '<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="projects.html" style="color:white; background-color: transparent;">Personal Projects<span class="caret"></span></a>'+
+                        '<ul class="dropdown-menu">'+
+                            '<li><a href="rps.html">RPS CTF</a></li>'+
+                            // '<li><a href="usefull.html">usefull files</a></li>'+
+                        '</ul>'+
+                    '</li>'+
+                    // '<li><a href="workRefs.html">Work Projects</a></li>'+
+                '</ul>'+
+            '</div>'+
         '</div>'+
-        '<div class="collapse navbar-collapse" id="myNavbar">'+
-            '<ul class="nav navbar-nav">'+
-                '<li><a href="index.html" style="color:black">About</a></li>'+
-                '<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="projects.html" style="color:black">Personal Projects<span class="caret"></span></a>'+
-                    '<ul class="dropdown-menu">'+
-                        '<li><a href="rps.html">RPS CTF</a></li>'+
-                        // '<li><a href="usefull.html">usefull files</a></li>'+
-                    '</ul>'+
-                '</li>'+
-                // '<li><a href="workRefs.html">Work Projects</a></li>'+
-            '</ul>'+
-        '</div>'+
-        
     '</div>'+
 '</div>'
 // '<br><br><br>'
@@ -46,4 +50,9 @@ const footer =''+
 function loadTemplate(){
     document.body.innerHTML += navbar;
     // document.body.innerHTML += footer;
+}
+
+
+function loadSocialBar(){
+    document.body.innerHTML += socialbar;
 }
