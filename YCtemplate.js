6d303a3148339f4aadf163e16aa2dbad8b3ee0be
Yair Charit <yaircharit@gmail.com>
2021-08-03 16:07:31 +0300

    [FEAT]  Timeline was added\n[FEAT]  Projects section update

--- YCtemplate.js
+++ YCtemplate.js
@@ -1,19 +1,14 @@
 //TODO: set styles using CSSClasses
-const socialbar =   '<table >'+
-                        '<tr style="height:70px">'+
-                            '<td ><a href="https://LinkedIn.com/in/yair-charit"><img id="linkedinIcon" src="Art/icons/white/linkedin.png" class="thumbnail img-rounded social-icon"></a></td>'+
-                            '<td><a href="mailto:yaircharit@gmail.com"><img id="emailIcon" src="Art/icons/white/email.png"class="thumbnail img-rounded social-icon"></a></td>'+
-                            '<td><a href="https://github.com/yaircharit"><img id="githubIcon" src="Art/icons/white/github.png"class="thumbnail img-rounded social-icon"></a></td>'+
-                            '<td><a href="tel:+972-54-440-8838"><img id="phoneIcon" src="Art/icons/white/phone.png" class="thumbnail img-rounded social-icon"></a></td>'+
-                            '<td><a href="https://api.whatsapp.com/send?phone=972544408838"><img id="whatsappIcon" src="Art/icons/white/whatsapp.png" class="thumbnail img-rounded social-icon"></a></td>'+
-                        '</tr>'+
-                    '</table>'
-
+const socialbar =   '<a style="padding:0; background-color: transparent;;" href="https://LinkedIn.com/in/yair-charit"><img id="linkedinIcon" src="Art/icons/white/linkedin.png" class="thumbnail img-rounded social-icon"></a>'+
+                    '<a style="padding:0;background-color: transparent;" href="mailto:yaircharit@gmail.com"><img id="emailIcon" src="Art/icons/white/email.png"class="thumbnail img-rounded social-icon"></a>'+
+                    '<a style="padding:0;background-color: transparent;" href="https://github.com/yaircharit"><img id="githubIcon" src="Art/icons/white/github.png"class="thumbnail img-rounded social-icon"></a>'+
+                    '<a style="padding:0;background-color: transparent;" href="tel:+972-54-440-8838"><img id="phoneIcon" src="Art/icons/white/phone.png" class="thumbnail img-rounded social-icon"></a>'+
+                    '<a style="padding:0;background-color: transparent;" href="https://api.whatsapp.com/send?phone=972544408838"><img id="whatsappIcon" src="Art/icons/white/whatsapp.png" class="thumbnail img-rounded social-icon"></a>'
 var isNavbarOpen = false;
 const navbar = ''+
 
 '<div id="navbar" class="navbar navbar-fixed-top" style="background-color: transparent;height: auto; position:fixed;">'+
-'<div id="navbarBackground" class="toggle-navbar colors heightAnimation navbar-collapsed " style="position:fixed; opacity:0; z-index:-99;"> </div>'+    
+    '<div id="navbarBackground" class="toggle-navbar colors heightAnimation navbar-collapsed " style="position:fixed; opacity:0; z-index:99;"> </div>'+    
     '<div class="container">'+
         // '<div id="navSocial" style="opacity: 0; margin-right:0; float:right">'+
         //     socialbar+
@@ -22,15 +17,15 @@ const navbar = ''+
             '<div class="navbar-header" >'+
                 // '<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>+
                 '<button type="button" id="toggler" class="navbar-toggle navbar-nav navbar-toggler-right" data-toggle="collapse" data-target=".navbar-collapse" style="margin-right:2%">'+
-                    '<span class="icon-bar" ></span>'+
-                    '<span class="icon-bar"></span>'+
-                    '<span class="icon-bar"></span>'+
+                    '<span class="white-bg icon-bar" ></span>'+
+                    '<span class="white-bg icon-bar"></span>'+
+                    '<span class="white-bg icon-bar"></span>'+
                 '</button>'+
                 '<a href="https://yaircharit.github.io" class="img-thumbnail img-circle" style="background-color:transparent; margin-top:5px"><img src="Art/YCLogo/YC_Logo_DarkRed2.png" alt="YCLogo" height="35px"/></a>'+
             '</div>'+
             '<div class="collapse navbar-collapse title" id="myNavbar">'+
                 '<ul id="toggle-navbar" class="nav navbar-nav ml-auto" style="width:94%">'+
-                    '<li id="navSocial" style="float:right; z-index:1000; margin-right:0">'+socialbar +'</li>'+
+                    '<li id="navSocial" class="horizontal" style="float:right; margin-right:0; opacity:0; display:flex;flex-direction: row;">'+socialbar +'</li>'+
                     '<li class="nav-item"><a href="index.html" style="color:white; background-color:transparent;">About</a></li>'+
                     '<li id="projectsDD" class="dropdown nav-item"><a class="dropdown-toggle" data-toggle="dropdown" href="projects.html" style="color:white; background-color: transparent;">Projects<span class="caret"></span></a>'+
                         '<ul class="dropdown-menu">'+
