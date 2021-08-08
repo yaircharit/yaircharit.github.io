63c781241b8d91212d2818205292628b6fd8cc59
Yair Charit <yaircharit@gmail.com>
2021-08-08 08:49:08 +0300

    [FEAT]  Added Mail Automation page

--- YCtemplate.js
+++ YCtemplate.js
@@ -1,42 +1,37 @@
+
 //TODO: set styles using CSSClasses
 const socialbar =   '<a style="padding:0; background-color: transparent;;" href="https://LinkedIn.com/in/yair-charit"><img id="linkedinIcon" src="Art/icons/white/linkedin.png" class="thumbnail img-rounded social-icon"></a>'+
                     '<a style="padding:0;background-color: transparent;" href="mailto:yaircharit@gmail.com"><img id="emailIcon" src="Art/icons/white/email.png"class="thumbnail img-rounded social-icon"></a>'+
                     '<a style="padding:0;background-color: transparent;" href="https://github.com/yaircharit"><img id="githubIcon" src="Art/icons/white/github.png"class="thumbnail img-rounded social-icon"></a>'+
                     '<a style="padding:0;background-color: transparent;" href="tel:+972-54-440-8838"><img id="phoneIcon" src="Art/icons/white/phone.png" class="thumbnail img-rounded social-icon"></a>'+
                     '<a style="padding:0;background-color: transparent;" href="https://api.whatsapp.com/send?phone=972544408838"><img id="whatsappIcon" src="Art/icons/white/whatsapp.png" class="thumbnail img-rounded social-icon"></a>'
-var isNavbarOpen = false;
-const navbar = ''+
 
+const navbar = ''+
 '<div id="navbar" class="navbar navbar-fixed-top" style="background-color: transparent;height: auto; position:fixed;">'+
     '<div id="navbarBackground" class="toggle-navbar colors heightAnimation navbar-collapsed " style="position:fixed; opacity:0; z-index:99;"> </div>'+    
     '<div class="container">'+
-        // '<div id="navSocial" style="opacity: 0; margin-right:0; float:right">'+
-        //     socialbar+
-        // '</div>'+
         '<div id="navbarContent" class="navbar navbar-fixed-top" style="margin-left:2%; height:auto">'+
             '<div class="navbar-header" >'+
-                // '<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>+
                 '<button type="button" id="toggler" class="navbar-toggle navbar-nav navbar-toggler-right" data-toggle="collapse" data-target=".navbar-collapse" style="margin-right:2%">'+
                     '<span class="white-bg icon-bar" ></span>'+
                     '<span class="white-bg icon-bar"></span>'+
                     '<span class="white-bg icon-bar"></span>'+
                 '</button>'+
-                '<a href="https://yaircharit.github.io" class="img-thumbnail img-circle" style="background-color:transparent; margin-top:5px"><img src="Art/YCLogo/YC_Logo_DarkRed2.png" alt="YCLogo" height="35px"/></a>'+
+                '<a href="index.html" class="img-thumbnail img-circle" style="background-color:transparent; margin-top:5px"><img src="Art/YCLogo/YC_Logo_DarkRed2.png" alt="YCLogo" height="35px"/></a>'+
             '</div>'+
             '<div class="collapse navbar-collapse title" id="myNavbar">'+
                 '<ul id="toggle-navbar" class="nav navbar-nav ml-auto" style="width:94%">'+
-                    '<li class="nav-item"><a href="index.html" style="color:white; background-color:transparent;">About</a></li>'+
-                    '<li id="projectsDD" class="dropdown nav-item"><a class="dropdown-toggle" data-toggle="dropdown" href="projects.html" style="color:white; background-color: transparent;">Projects<span class="caret"></span></a>'+
+                    '<li class="nav-item"><a href="index.html" class="whiteText">About</a></li>'+
+                    '<li id="projectsDD" class="dropdown nav-item"><a class="dropdown-toggle whiteText" data-toggle="dropdown" href="projects.html"">Projects<span class="caret"></span></a>'+
                         '<ul class="dropdown-menu">'+
-                            '<li><a href="rps.html">RPS CTF</a></li>'+
-                            '<li><a href="bookmarks.html" target="blank">Dashboard+</a></li>'+
+                            '<li><a class="grayText" href="rps.html">RPS CTF</a></li>'+
+                            '<li><a class="grayText" href="bookmarks.html">Dashboard+</a></li>'+
+                            '<li><a class="grayText" href="autoMail.html">Mail Automation</a></li>'+
                             // '<li><a href="usefull.html">usefull files</a></li>'+
                         '</ul>'+
                     '</li>'+
-                    '<li class="nav-item"><a href="Yair Charit - CVMay2021.pdf" target="blank" style="color:white; background-color:transparent;">Resume</a></li>'+
-                    '<li id="navSocial" class="horizontal" style="float:right; margin-right:0; opacity:0; display:flex;flex-direction: row;">'+socialbar +'</li>'+
-                    
-                    // '<li><a href="workRefs.html">Work Projects</a></li>'+
+                    '<li class="nav-item"><a class="whiteText"  href="Yair Charit - CVMay2021.pdf" target="blank">Resume</a></li>'+
+                    '<li id="navSocial" class="horizontal">'+socialbar +'</li>'+
                 '</ul>'+
 
             '</div>'+
@@ -44,20 +39,51 @@ const navbar = ''+
     '</div>'+
 '</div>'
 
-const footer =''+
-'<ul class="sidenav">'+
-    // '<hr/>'+
-    // '<li>Contact me:</li>'+
-    '<li><a class="sidenav-object" href="https://www.linkedin.com/in/yair-charit"><img src="Art/icons/linkedin.png" height="30" /></a></li>'+
-    '<li><a class="sidenav-object" href="mailto:yaircharit@gmail.com"><img src="Art/icons/email.png" height="30" /></a></li>'+
-    '<li><a class="sidenav-object" href="tel:+97254-440-8838"><img src="Art/icons/phone.png" height="30" /></a></li>'+
-'</ul>'
+var isNavbarOpen = false
+var minHeight, maxHeight
 function loadTemplate(){
     document.body.innerHTML += navbar;
-    // document.body.innerHTML += footer;
-}
+    
+    let navBG = document.getElementById('navbarBackground');
+    let navContent = document.getElementById('navbarContent');
+    let toggler = document.getElementById('toggler');
+    let projDD = document.getElementById('projectsDD')
+    minHeight = navContent.offsetHeight
+    toggler.click()
+    maxHeight = navContent.offsetHeight
+    toggler.click()
+    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent) || window.innerWidth <= 768) {
+        navSocial.style.float = "left"   
+    }
+    else
+    {
+        navSocial.style.float = "right"
+    }
+    
+    toggler.onclick = function () {
+        console.log('height2: ', maxHeight)
+        isNavbarOpen = !isNavbarOpen;
+        if (isNavbarOpen) {
+            navBG.style.maxHeight = `${maxHeight}px`
+        }
+        else {
+            navBG.style.maxHeight = `${minHeight}px`
+            isProjOpen = false
+        }
+        return;
+    }   
 
-
-function loadSocialBar(){
-    document.body.innerHTML += socialbar;
+    var isProjOpen = false;
+    projDD.onclick =  function () {
+        if (isNavbarOpen) {
+            isProjOpen = !isProjOpen
+            if (isProjOpen) {
+                navBG.style.maxHeight = `${maxHeight + projDD.offsetHeight + 45}px`
+            }
+            else {
+                navBG.style.maxHeight = `${maxHeight}px`
+            }
+        }
+    }
+    
 }
