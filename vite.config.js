a59de7590f50283d19c6ee365214649f0521bb96
yaircharit <yaircharit@gmail.com>
2025-06-12 20:17:45 +0300

    feat: add .nojekyll file and update homepage in package.json

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/yaircharit.github.io/',
+  base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    assetsDir: 'assets'
   }
 })
