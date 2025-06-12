9d6a81eb9af28227e4534c70b00ceec59596c0d1
yaircharit <yaircharit@gmail.com>
2025-06-12 16:14:53 +0300

    git react try 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx' 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
